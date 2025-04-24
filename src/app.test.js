30c5dc092fcfb636b99c205e7129036552165619
MasterTintin <6733031821@student.chula.ac.th>
2025-04-24 14:48:19 +0700

    update app.test.js

--- src/app.test.js
+++ src/app.test.js
@@ -1,12 +1,12 @@
 const { dayOfTheWeek } = require('./app');
 const { monthOfTheYear } = require('./app');
 
-
 test('getDay returns the long-format day of the week', () => {
-    const day = dayOfTheWeek( new Date('3/11/2020') );
-    expect( day ).toBe('Wednesday');
+    const day = dayOfTheWeek(new Date('3/11/2020'));
+    expect(day).toBe('Wednesday');
 });
-test('getDay returns the month of the year', () => {
-    const month = monthOfTheYear( new Date('3/11/2020') );
-    expect( month ).toBe('November');
+
+test('getMonth returns the correct month of the year', () => {
+    const month = monthOfTheYear(new Date('11/11/2020'));
+    expect(month).toBe('November');
 });
